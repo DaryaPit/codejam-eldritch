5bfa2e90687db58b587bb300d51cb528a1708608
DaryaPit <m-dv@tut.by>
2022-08-24 16:40:53 +0300

    init: codejam-eldritch

---
+++ data/difficulties.js
@@ -0,0 +1,26 @@
+const difficulties = [
+  {
+    id: 'very_easy',
+    name: 'Очень низкая',
+    antonim: 'hard'
+  },
+  {
+    id: 'easy',
+    name: 'Низкая'
+  },
+  {
+    id: 'normal',
+    name: 'Средняя'
+  },
+  {
+    id: 'hard',
+    name: 'Высокая'
+  },
+  {
+    id: 'very_hard',
+    name: 'Очень высокая',
+    antonim: 'easy'
+  },
+]
+
+export default difficulties
